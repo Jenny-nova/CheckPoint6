04a4d7f8a10beb6aeace8e3481c2f76a102f2d15
Jenny-nova <jeniferfernandezares@gmail.com>
2025-04-03 16:49:33 +0200

    Delete CheckPoint6 directory

--- CheckPoint6/ejercicio.py
+++
@@ -1,9 +0,0 @@
-class Usuario:
-    def __init__(self, nombre_usuario, contraseña):
-        self.nombre_usuario = nombre_usuario
-        self.contraseña = contraseña
-        
-usuario_uno = Usuario ("Nova", "Password")
-
-print(usuario_uno.nombre_usuario)
-print(usuario_uno.contraseña)
